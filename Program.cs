68dda37f6a67260645ebdd380b189feb2cbf4c6e
Даниил Полянский <dm.tsiganov@gmail.com>
2022-12-04 02:36:24 +0700

    64

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void PrintRange (int N)
+{
+  if (N == 0) 
+  {
+    return;
+  }
+     if (N == 1)
+     {
+      Console.Write(N);
+     }    
+    else
+    Console.Write(N + ",");
+    PrintRange (N - 1);
+}
+
+PrintRange(N);
