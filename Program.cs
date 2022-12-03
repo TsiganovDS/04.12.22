e6d82c38b58ac9664a0579f0c46aadbe8b2627a9
Даниил Полянский <dm.tsiganov@gmail.com>
2022-12-04 03:05:50 +0700

    66

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers (int M, int N)
+{
+    if (M == N)
+    {
+        return N;
+    }
+    else 
+    {
+        N += SumNumbers (M, N-1);
+    }
+    return N;
+}
+
+Console.Write ($"Сумма чисел от {M} до {N} - {SumNumbers (M, N)}");
