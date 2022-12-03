ea57e5c4b2a0574c6aeae269c26dfb7ae3fe7f9f
Даниил Полянский <dm.tsiganov@gmail.com>
2022-12-04 03:25:38 +0700

    68

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int funсtionAkkerman(int M, int N)
+{
+    if (M == 0)
+    {
+        return N + 1;
+    }
+    else if (M > 0 && N == 0)
+    {
+        N = funсtionAkkerman(M - 1, 1);
+        return N;
+    }
+    else if (M > 0 && N > 0)
+    {
+        N = funсtionAkkerman(M, N - 1);
+        N = funсtionAkkerman(M - 1, N);
+    }
+    return N;
+}
+
+Console.WriteLine ($"A({M},{N}) = {funсtionAkkerman(M, N)}");
